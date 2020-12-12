e258ffb003a4c33ee055a5736a07a5fb20fe7c93
Ridhima345 <73940377+Ridhima345@users.noreply.github.com>
2020-12-12 12:43:15 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,38 @@
-
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-function preload()
-{
-	
-}
-
+var car, wall
+var weight, speed
 function setup() {
-	createCanvas(800, 700);
+  createCanvas(1600,400);
 
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-
-
-	Engine.run(engine);
+  speed=random(55,90)
+  weight=random(400,1500)
   
-}
+  car = createSprite(50, 200, 50, 50);
+  car.velocityX=speed;
+  car.shapeColor=color(255);
 
+  wall= createSprite(1500, 200, 60,height/2);
+  wall.shapeColor=color(80,80,80);
+}
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
+  background(0); 
+
+  if (wall.x-car.x<(car.width+wall.width)/2){
+   car.velocityX=0;   
+  var deformation=0.5 * weight * speed * speed/22509;
  
+  if (deformation>180){
+   car.shapeColor=color(255,0,0);
+  }
+
+  if (deformation<180 && deformation>100){
+    car.shapeColor=color(230,230,0);
+   }
+
+   if (deformation<100){
+    car.shapeColor=color(0,255,0);
+   }
+
+  }
+  drawSprites();
 }
-
-
-
